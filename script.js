6dbe8c706046bbf54ff4c2f6ea823d46ae217385
DrRen <33030591+DrRen@users.noreply.github.com>
2018-03-16 17:15:32 +0700

    1

--- script.js
+++ script.js
@@ -1,40 +1,32 @@
 var images = new Array()
+var imagesCount = 1164;
 var progress_circle = $(".my-progress-bar").gmpc({
-        line_width: 6,
-        color: "#1ABC9C",
-        starting_position: 0,
-        percent: 0,
-        percentage: true,
-        text: "Loading..."
-    });
+  line_width: 6,
+  color: "#1ABC9C",
+  starting_position: 0,
+  percent: 0,
+  percentage: true,
+  text: "Loading..."
+});
 
 function preload(path, max) {
-  var index = max+"";
+  var index = max + "";
   for (var i = 0; i < index; i++) {
     document.clear;
     //console.log("загрузка "+(i+1)+" из "+ index)
 
-    progress_circle.gmpc('animate', i/index*100, 3000);
+    progress_circle.gmpc('animate', i / index * 100, 3000);
 
 
     //
     images[i] = new Image();
     var zeros = "";
-    for (var j = 0; j < 6 - (i+"").length; j++) {
+    for (var j = 0; j < 6 - (i + "").length; j++) {
       zeros += "0";
     }
-    images[i].src = path + zeros + i+".jpeg";
+    images[i].src = path + zeros + i + ".jpeg";
   }
-  wrapper.style.display="block";
-  mpb.style.display="none";
-  $("#wrapper").smoothWheel();
-}
-preload("60_428x240/ultraLow_", 1164)
 
-wrapper.onscroll = function() {
-  container.style.background = 'url('+images[wrapper.scrollTop].src+') no-repeat';
-  container.style.backgroundPositionY = wrapper.scrollTop + "px";
-  container.style.backgroundSize = "100%";
 }
 
 function getImageIndex() {
@@ -44,4 +36,21 @@ function getImageIndex() {
     zeros += "0";
   }
   return zeros + index;
+}
+
+function unlockOnLoad() {
+  if (images.length>=imagesCount-1) {
+    wrapper.style.display = "block";
+    mpb.style.display = "none";
+    $("#wrapper").smoothWheel();
+  }
+
+}
+
+preload("60_428x240/ultraLow_", imagesCount);
+
+wrapper.onscroll = function() {
+  container.style.background = 'url(' + images[wrapper.scrollTop].src + ') no-repeat';
+  container.style.backgroundPositionY = wrapper.scrollTop + "px";
+  container.style.backgroundSize = "100%";
 }
